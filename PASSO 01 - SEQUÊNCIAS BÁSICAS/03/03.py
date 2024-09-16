5cb985de021ce2edd7b3548d269fd825359f0c35
Diego Rosas <diegorosas19@gmail.com>
2024-09-16 20:07:38 -0300

    Exercicio 03

---
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/03/03.py
@@ -0,0 +1,12 @@
+# Exercicio 03
+# Funcao : Lê o nome e o salário de um funcionário, mostrando no final uma mensagem.
+# Autor : Diego Rosas
+# Data : 16/09/2024
+# Seção de Declarações 
+
+nomeDoFuncionario = input('Digite o nome do funcionario(a): ')
+salarioDoFuncionario = int(input('Digite o salario do funcionario(a): '))
+mesDoSalario = input('Digite o mês correspodente ao salario: ')
+
+
+print(f"O funcionario(a), {nomeDoFuncionario}, recebeu um salário de R${salarioDoFuncionario},00 em {mesDoSalario}.")
