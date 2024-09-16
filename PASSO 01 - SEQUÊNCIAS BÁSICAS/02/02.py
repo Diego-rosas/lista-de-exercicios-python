d63eee88b2cac520e59917d36a85533779e66063
Diego Rosas <diegorosas19@gmail.com>
2024-09-16 19:49:06 -0300

    Exercicio 02

---
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/02/02.py
@@ -0,0 +1,8 @@
+# Funcao : Ler o nome de uma pessoa e dar boas vindas.
+# Autor : Diego Rosas
+# Data : 16/09/2024
+
+
+nome = input("Qual é o seu nome? ")
+
+print(f'Olá, {nome}, é um prazer te conhecer!')
