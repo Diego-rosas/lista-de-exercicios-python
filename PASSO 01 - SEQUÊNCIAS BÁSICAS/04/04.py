172236d7361abe2b3fedf61f66b199985be911dc
Diego Rosas <diegorosas19@gmail.com>
2024-09-17 22:24:32 -0300

    Exercicio 04

---
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/04/04.py
@@ -0,0 +1,12 @@
+# Exercicio 04
+# Função : Algoritmo que lê dois números inteiros e mostra o somatório entre eles.
+# Autor : Diego Rosas
+# Data : 17/09/2024
+# Seção de Declarações 
+
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+
+soma = valor1 + valor2
+
+print(f'A soma entre {valor1} e {valor2} é igual a {soma}.')
