b144cc486d62c065a5b061f9b63e81d4b57bd31d
Diego Rosas <diegorosas19@gmail.com>
2024-09-17 22:35:11 -0300

    Exercicio 05

---
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/05/05.py
@@ -0,0 +1,12 @@
+# Exercicio 05
+# Função : Calcula a media de duas notas e mostra o resultado.
+# Autor : Diego Rosas
+# Data : 17/09/2024
+# Seção de Declarações 
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'A média entre {nota1} e {nota2} é igual a {media}')
