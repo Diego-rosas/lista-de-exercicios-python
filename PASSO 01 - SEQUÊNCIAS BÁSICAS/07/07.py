be3b1b70e286a9b7e5811f5d2de40635a8061ced
Diego Rosas <diegorosas19@gmail.com>
2024-09-18 23:00:50 -0300

    Exercicio 07

---
+++ PASSO 01 - SEQUÊNCIAS BÁSICAS/07/07.py
@@ -0,0 +1,13 @@
+# Exercicio 07
+# Função : Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a sua terça parte.
+# Autor : Diego Rosas
+# Data : 18/09/2024
+# Seção de Declarações
+
+numero = float(input('Digite um número: '))
+
+dobro = numero * 2
+tercaParte = numero/3
+
+print(f'O dobro de {numero} é {dobro}')
+print(f'O terça parte de {numero} é {tercaParte}')
